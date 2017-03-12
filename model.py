2ba8c5abd796ae30aae1192dbcda3326897f5753
Bogdan Oloeriu <bogdan.oloeriu@gmail.com>
2017-03-12 06:02:30 +0100

    added drop layer

--- model.py
+++ model.py
@@ -4,7 +4,7 @@ import csv
 samples = []
 
 #my root folder for the data
-dataFolder = "/home/bogdan/colected_data/session3"
+dataFolder = "/home/bogdan/colected_data/session6"
 
 with open(dataFolder + '/driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
@@ -45,36 +45,38 @@ validation_generator = generator(validation_samples, batch_size=32)
 
 
 from keras.models import Sequential, Model
-from keras.layers import Flatten, Dense, Lambda, Cropping2D
+from keras.layers import Flatten, Dense, Lambda, Cropping2D, Dropout
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
-
-
 import matplotlib.pyplot as plt
 
 #define the model
-model = Sequential()
-model.add(Lambda(lambda x: x/255.0 - 0.5, input_shape=(160,320,3)))
-model.add( Cropping2D(cropping=((70,25), (0,0))))
-model.add(Convolution2D(24,5,5,subsample=(2,2),activation="relu"))
-model.add(Convolution2D(36,5,5,subsample=(2,2),activation="relu"))
-model.add(Convolution2D(48,5,5,subsample=(2,2),activation="relu"))
+def define_network():
+    model = Sequential()
+    model.add(Lambda(lambda x: x/255.0 - 0.5, input_shape=(160,320,3)))
+    model.add( Cropping2D(cropping=((70,25), (0,0))))
+    model.add(Dropout(0.1))
+    model.add(Convolution2D(24,5,5,subsample=(2,2),activation="relu"))
+    model.add(Convolution2D(36,5,5,subsample=(2,2),activation="relu"))
+    model.add(Convolution2D(48,5,5,subsample=(2,2),activation="relu"))
 
-model.add(Convolution2D(64,3,3,activation="relu"))
-model.add(Convolution2D(64,3,3,activation="relu"))
-model.add(Flatten())
-model.add(Dense(100))
-model.add(Dense(50))
-model.add(Dense(10))
-model.add(Dense(1))
+    model.add(Convolution2D(64,3,3,activation="relu"))
+    model.add(Convolution2D(64,3,3,activation="relu"))
+    model.add(Flatten())
+    model.add(Dense(100))
+    model.add(Dense(50))
+    model.add(Dense(10))
+    model.add(Dense(1))
 
-model.compile(loss='mse', optimizer='adam')
+    model.compile(loss='mse', optimizer='adam')
+    return model
 
 #train the model
+model = define_network()
 history_object = model.fit_generator(train_generator,
                         samples_per_epoch = len(train_samples),
                         validation_data = validation_generator,
-                        nb_val_samples = len(validation_samples), nb_epoch=7)
+                        nb_val_samples = len(validation_samples), nb_epoch=6)
 
 model.save('model.h5')
 
